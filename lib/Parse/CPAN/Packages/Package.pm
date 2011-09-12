0ed4d135d40b7339fbb7c06479a12e51abd759f4
Christian Walde <walde.christian@googlemail.com>
2011-09-12 21:51:46 +0200

    added rudimentary logic to return the subs in a package, as well as verify via regex whether a given sub is in the package

--- lib/Parse/CPAN/Packages/Package.pm
+++ lib/Parse/CPAN/Packages/Package.pm
@@ -1,6 +1,8 @@
 package Parse::CPAN::Packages::Package;
 use Moose;
 
+use PPI;
+
 has 'package'      => ( is => 'rw', isa => 'Str' );
 has 'version'      => ( is => 'rw', isa => 'Str' );
 has 'prefix'       => ( is => 'rw', isa => 'Str' );
@@ -22,6 +24,23 @@ sub file_content {
     my $filename = $self->filename;
     my $content  = $self->distribution->get_file_from_tarball( $filename );
     return $content;
+}
+
+sub subs {
+    my ( $self ) = @_;
+
+    my $document = PPI::Document->new( \( $self->file_content ) );
+    my $subs = $document->find('PPI::Statement::Sub');
+
+    return map { $_->name } @{$subs};
+}
+
+sub has_matching_sub {
+    my ( $self, $sub_regex ) = @_;
+
+    my @matching_subs = grep { $_ =~ $sub_regex } $self->subs;
+
+    return @matching_subs;
 }
 
 __PACKAGE__->meta->make_immutable;
@@ -50,3 +69,12 @@ the files contained in the distribution it belongs to.
 =head2 file_content
 
 Tries to return the contents of the file returned by filename().
+
+=head2 subs
+
+Experimental function. Tries to return the names of all subs in the package.
+
+=head2 has_matching_sub( $regex )
+
+Experimental function. Tries to see if any sub name in the package matches the
+regex.
